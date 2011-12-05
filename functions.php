24c86f6af865f1f0166a007a09b0d348f03143b1
Sam Reigle <sreigle@ackmanndickenson.com>
2011-12-05 15:14:51 -0600

    Added function to add first and last classes on wp_menus

--- functions.php
+++ functions.php
@@ -15,6 +15,13 @@ endif;
 
 add_editor_style( 'editor-style.css' );
 
+function add_first_and_last($output) {
+  $output = preg_replace('/class="menu-item/', 'class="first-menu-item menu-item', $output, 1);
+  $output = substr_replace($output, 'class="last-menu-item menu-item', strripos($output, 'class="menu-item'), strlen('class="menu-item'));
+  return $output;
+}
+add_filter('wp_nav_menu', 'add_first_and_last');
+
 function my_init_method() {
   if (is_admin() == false ):
     wp_deregister_script( 'jquery' );
