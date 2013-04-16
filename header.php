fe4220129c29ed9d29485471f8afaba80052d318
Joe Soucheray <jsoucheray@ackmanndickenson.com>
2013-04-16 13:18:42 -0500

    added mobile to header

--- header.php
+++ header.php
@@ -1,6 +1,10 @@
 <!DOCTYPE html>
 <html <?php language_attributes(); ?>>
 <head>
+<!--
+UNCOMMENT THIS OUT TO USE WITH BOOTSTRAP CSS IN STYLES.CSS
+<meta name="viewport" content="width=device-width, initial-scale=1.0">
+-->
 <meta charset="<?php bloginfo( 'charset' ); ?>" />
 <title>
   <?php
@@ -17,7 +21,7 @@
 <body <?php body_class(); ?>>
 
 <a href="<?php echo home_url( '/' ); ?>">Home Page</a>
-<?php 
+<?php
   // Uncomment to show menu
   // wp_nav_menu( array( 'menu' => 'Main' ) );
 ?>
