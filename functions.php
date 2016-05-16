9ae2a2a72faf35dda9b170790ae24ebdc11a2e1c
Sam Reigle <sreigle@ackmanndickenson.com>
2016-05-16 17:11:42 -0500

    update sidebar registeration

--- functions.php
+++ functions.php
@@ -1,17 +1,19 @@
 <?php
 
-register_nav_menus( array('primary' => 'Primary Menu') );
-register_sidebar( array('name' => 'Sidebar') );
+register_nav_menus([ 'primary' => 'Primary Menu']);
+
+register_sidebar([
+  'id'   => 'sidebar-1',
+  'name' => 'Sidebar'
+]);
 
 add_theme_support( 'automatic-feed-links' );
 
 function my_init_method() {
-  if(!is_admin()) {
+  if( !is_admin() ) {
     wp_enqueue_script( 'jquery' );
     wp_register_style( 'global', get_bloginfo('template_directory') . '/css/global.css');
     wp_enqueue_style( 'global' );
   }
 }
-add_action('init', 'my_init_method');
-
-?>
+add_action( 'init', 'my_init_method' );
